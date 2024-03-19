a66d378e77d13d06e879e6f43b7e566ef0065756
Rimalkhan <159910937+Rimalkhan@users.noreply.github.com>
2024-03-19 06:33:20 +0500

    Add files via upload
    Assignment question 1 to 5.

---
+++ index.js
@@ -0,0 +1,21 @@
+//ANSWER #1
+// Personal Message
+let username = "Eric";
+console.log(`Hello ${username}, would you like to learn some TypeScript today?`);
+//ANSWER #2
+// Name Cases
+let personName = "John Doe";
+console.log("Lowercase:", personName.toLowerCase());
+console.log("Uppercase:", personName.toUpperCase());
+console.log("Titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+//ANSWER #3
+// Famous Quote
+let famousquote = "A person who never made a mistake never tried anything new.";
+let author = "Albert Einstein";
+console.log(`${author} once said, "${famousquote}"`);
+//ANSWER #4
+// Famous Quote 2
+let famousquote2 = "A person who never made a mistake never tried anything new.";
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "${famousquote2}"`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,21 @@
+//ANSWER #1
+// Personal Message
+let username: string = "Eric";
+console.log(`Hello ${username}, would you like to learn some TypeScript today?`);
+ //ANSWER #2
+ // Name Cases
+let personName: string = "John Doe";
+console.log("Lowercase:", personName.toLowerCase());
+console.log("Uppercase:", personName.toUpperCase());
+console.log("Titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+//ANSWER #3
+// Famous Quote
+let famousquote: string = "A person who never made a mistake never tried anything new.";
+let author: string = "Albert Einstein";
+console.log(`${author} once said, "${famousquote}"`);
+//ANSWER #4
+// Famous Quote 2
+let famousquote2: string = "A person who never made a mistake never tried anything new.";
+let famous_person: string = "Albert Einstein";
+let message: string = `${famous_person} once said, "${famousquote2}"`;
+console.log(message);
